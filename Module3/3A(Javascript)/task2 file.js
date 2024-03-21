faf49560ba9f19612af2ad36cbd4b19161ee4515
javascriptcourse <eevwrujae@gmail.com>
2024-03-21 10:55:41 +0100

    java fun facts

---
+++ Module3/3A(Javascript)/task2 file.js
@@ -0,0 +1,5 @@
+/***** FUN FACTS ABOUT JAVASCRIPT
+ * 1. I always thought the semi colon at the end were compulsory
+ * 2. I also use to think that Javascript was very very difficult and that Java was easier and more acceptable.
+ * 3. Javascript is totally not related to Java in anyway.
+ * ****/
