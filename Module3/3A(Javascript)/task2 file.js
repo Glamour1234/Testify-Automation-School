e0fd311006e2cd3debd4d4fadbd35f9c4d5d7bf2
javascriptcourse <eevwrujae@gmail.com>
2024-03-21 11:14:43 +0100

    fun facts

--- Module3/3A(Javascript)/task2 file.js
+++ Module3/3A(Javascript)/task2 file.js
@@ -2,5 +2,5 @@
  * 1. I always thought the semi colon at the end were compulsory
  * 2. I also use to think that Javascript was very very difficult and that Java was easier and more acceptable.
  * 3. Javascript is totally not related to Java in anyway.
- * ****/
+ * ************/
  
