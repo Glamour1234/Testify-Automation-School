a39fb97fccfd875c517bbb2e749d5e8d0fb3fcb4
Amazon-Coding-Challenge <eevwrujae@gmail.com>
2024-05-17 07:45:03 +0100

    task4

---
+++ Module4/4A/TestifyCourse/src/com/welcometojava/BAsicArithmetic.java
@@ -0,0 +1,18 @@
+package com.welcometojava;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //basic arithmetic
+        int age1 = 20;
+        int age2 = 30;
+        System.out.println(age1+age2);
+
+        //incremental and decremental
+        System.out.println(age1++);
+
+        //compund assignment operator; +=, /= etc
+        age1 -= age2;
+        System.out.println(age1);
+
+    }
+}
