bfa59cc5721ba7788fb9e50897db3165aa4933c4
Amazon-Coding-Challenge <eevwrujae@gmail.com>
2024-05-17 07:14:43 +0100

    task1

--- Module4/4A/TestifyCourse/src/com/welcometojava/HelloWorld.java
+++ Module4/4A/TestifyCourse/src/com/welcometojava/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         //Hello World print statement
-        System.out.println("Hello World!");
+        System.out.println("Hello World! Welcome to my latest adventure with Java");
     }
 }
