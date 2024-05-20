61ca3b52f28d16ebf8ac9e3f002ba466ce45ac51
Amazon-Coding-Challenge <eevwrujae@gmail.com>
2024-05-20 12:24:49 +0100

    task9B

---
+++ Module4/4A/TestifyCourse/src/com/welcometojava/task9B.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class task9B {
+    public static void main(String[] args)
+    {
+        //while loop
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        while(userInput != "testify"){
+            System.out.println("try again");
+            userInput = scanner.nextln();
+        }else{
+        System.out.println("correct input");
+    }
+
+        }
+
+
+
+}
+
+
+
