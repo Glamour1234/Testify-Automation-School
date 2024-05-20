cdd6b602cf305852bf5e201838d8d016abb52e77
Amazon-Coding-Challenge <eevwrujae@gmail.com>
2024-05-20 11:37:47 +0100

    task4 update

--- Module4/4A/TestifyCourse/src/com/welcometojava/task4.java
+++ Module4/4A/TestifyCourse/src/com/welcometojava/task4.java
@@ -3,9 +3,10 @@ package com.welcometojava;
 public class task4 {
     public static void main(String[] args)
     {
+        //calculate area of a circle
         float radius = 21.7;
-        float area = 3.14 * Math.pow(radius, 2);
-        System.out.println(area + "m");
+        float areaOfACircle = 3.14 * Math.pow(radius, 2);
+        System.out.println(areaOfACircle + "m");
 
     }
 }
